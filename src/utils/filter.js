b3ddf680285aaa90aab49b642a26c0e90c0bbc5b
hoon2hooni <sanghoon.engineer@gmail.com>
2023-02-06 08:29:01 +0900

    imp: make filter fn inversion of control

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,24 +1,10 @@
-export default function filter(
-  array,
-  {
-    filterNull = true,
-    filterUndefined = true,
-    filterEmptyString = false,
-    filterZero = false,
-  } = {}
-) {
+export default function filter(array, filterFn) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
-    if (
-      (filterNull && element === null) ||
-      (filterUndefined && element === undefined) ||
-      (filterEmptyString && element === "") ||
-      (filterZero && element === 0)
-    ) {
-      continue;
+    if (filterFn(element)) {
+      newArray.push(element);
     }
-    newArray.push(element);
   }
   return newArray;
 }
--- src/utils/filter.test.js
+++ src/utils/filter.test.js
@@ -2,12 +2,18 @@ import { describe, it, expect } from "vitest";
 import filter from "./filter";
 describe("filter", () => {
   it("should return an array", () => {
-    const result = filter([1, 2, 3, null, undefined]);
+    const result = filter(
+      [1, 2, 3, null, undefined],
+      (el) => el !== null && el !== undefined
+    );
     expect(result).toEqual([1, 2, 3]);
   });
 
   it("should return an array", () => {
-    const result = filter([1, 2, 3, null, undefined, ''],{filterEmptyString: true});
+    const result = filter(
+      [1, 2, 3, null, undefined, ""],
+      (el) => el !== null && el !== undefined && el !== ""
+    );
     expect(result).toEqual([1, 2, 3]);
   });
 });
